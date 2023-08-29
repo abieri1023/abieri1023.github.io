b6b5033d8dcd00f0c7db3ed937881921605a2f35
abieri <1459007075@qq.com>
2023-08-29 12:48:57 +0800

    Push Code:2023/08/29,12:48:57

--- autoPull&Push.bat
+++ autoPull&Push.bat
@@ -5,12 +5,14 @@ set /p command="pull or push?: "
 if "%command%"=="pull" (
 	echo pull
 	git pull
+	echo pullOK
 	pause>null
 ) else if "%command%"=="push" (
 	echo push
 	git add .
 	git commit -m "Push Code:%date:~0,10%,%time:~0,8%"
 	git push origin main
+	echo pushOK
 	pause>null
 ) else (
 	echo ???
