50c519d14719fd6f6f813503a1d2d69d0001a012
杨晨 <1459007075@qq.com>
2023-10-07 10:49:44 +0800

    Push Code:2023/10/07,10:49:44

--- autoPull&Push.bat
+++ autoPull&Push.bat
@@ -6,16 +6,16 @@ if "%command%"=="pull" (
 	echo pull
 	git pull
 	echo pullOK
-	pause>null
+	pause>nul
 ) else if "%command%"=="push" (
 	echo push
 	git add .
 	git commit -m "Push Code:%date:~0,10%,%time:~0,8%"
 	git push origin main
 	echo pushOK
-	pause>null
+	pause>nul
 ) else (
 	echo ???
-	pause>null
+	pause>nul
 )
 
